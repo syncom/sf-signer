f2bfd1f0241459d284461110deadedd12fee690c
Ning Shang <syncom.dev@gmail.com>
2022-06-20 22:30:36 -0700

    Add sanity checks in CI
    Building, unit testing, and integrationt testing.

---
+++ script/sign-and-verify.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly SCRIPT_DIR
+
+PROJECT_ROOT="${SCRIPT_DIR}/.."
+readonly PROJECT_ROOT
+
+CERT_FILE="${PROJECT_ROOT}/test/certificate.pem"
+readonly CERT_FILE
+
+SFSIGNER_EXE="${PROJECT_ROOT}/build/sfsigner"
+
+export SFSIGNER_PRIVATE_KEY="$(cat "${PROJECT_ROOT}/test/private.key")"
+
+for i in $(seq 20); do
+  len=$((2**i))
+  payload=$(mktemp)
+  echo "Generate random payload of ${len} bytes in ${payload}"
+  head -c "${len}" /dev/urandom > "${payload}"
+
+  echo "Sign payload"
+  sig=$(mktemp)
+
+  "${SFSIGNER_EXE}" sign -c "${CERT_FILE}" "${payload}" > "${sig}"
+  echo "Signature is written to ${sig}"
+
+  echo "Verify payload against good signature"
+  "${SFSIGNER_EXE}" verify -p "${payload}" -s "${sig}" -c "${CERT_FILE}"
+
+  echo "Verify payload against bad signature"
+  echo "A" >> "${payload}"
+  if ! "${SFSIGNER_EXE}" verify -p "${payload}" -s "${sig}" -c "${CERT_FILE}"; then
+    echo "Tampered payload failed signature verification as expected"
+  else
+    exit 1
+  fi
+
+  rm "${payload}" "${sig}"
+done
+
+unset SFSIGNER_PRIVATE_KEY
